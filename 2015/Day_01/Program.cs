954b2ad3bc4c8108fa243b4e6ba4514afefccdcf
Caro <carolines-git@xirb.de>
2022-12-13 22:49:21 +0100

    Completed day 1 and day 2 of year 2015 Advent of Code

---
+++ 2015/Day_01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+StreamReader sr = new StreamReader("201501_Input.txt", Encoding.UTF8);
+
+string txt = sr.ReadToEnd();
+sr.Close();
+
+int etage = 0;
+bool firstBasementEntry = false;
+int valueFirstBasementEntry = 0;
+
+foreach (char letter in txt)
+{
+    if (letter.Equals('('))
+    {
+        etage++;
+    }
+
+    if (letter.Equals(')'))
+    {
+        etage--;
+    }
+
+    if (!firstBasementEntry)
+    {
+        if (etage == -1)
+        {
+            firstBasementEntry = true;
+        }
+
+        valueFirstBasementEntry++;
+    }
+}
+
+Console.WriteLine("Er befindet sich in Stockwerk: " + etage);
+Console.WriteLine("Zum ersten mal betritt er das erste Untergeschoss nach: {0} Schritten", valueFirstBasementEntry);
---
+++ 2015/Day_02/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+string line = "";
+string[] temp = new string[3];
+int wrappingPaper = 0;
+int ribbon = 0;
+
+StreamReader sr = new StreamReader("201502_Input.txt", Encoding.UTF8);
+line = sr.ReadLine();
+
+try
+{
+    while (line != null)
+    {
+        temp = line.Split('x', 3);
+
+        wrappingPaper += WrappingPaper(int.Parse(temp[0]), int.Parse(temp[1]), int.Parse(temp[2]));
+        ribbon += Ribbon(int.Parse(temp[0]), int.Parse(temp[1]), int.Parse(temp[2]));
+        line = sr.ReadLine();
+    }
+    sr.Close();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+
+Console.WriteLine($"You need {wrappingPaper} square feet of wrapping paper");
+Console.WriteLine($"You need {ribbon} feet of ribbon");
+
+
+static int WrappingPaper(int length, int width, int height)
+{
+    int[] square = new int[3];
+    square[0] = length * width;
+    square[1] = length * height;
+    square[2] = width * height;
+
+    int slack = square.Min();
+    
+    return square.Sum() * 2 + slack;
+}
+
+static int Ribbon(int length, int width, int height)
+{
+    int[] size = new int[3];
+    size[0] = (length + width) * 2;
+    size[1] = (length + height) * 2;
+    size[2] = (width + height) * 2;
+
+    int wrap = size.Min();
+    int bow = length * width * height;
+    return wrap + bow;
+}
+
